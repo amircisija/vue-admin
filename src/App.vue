cc33a80ee8bf7a57a9d5b0cbbae1c45f2c295230
Amir <amircisija@gmail.com>
2020-08-15 20:35:22 +0200

    setting up base of project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app app>
+    <AppNavigation />
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <transition name="fade" mode="out-in">
+              <router-view></router-view>
+            </transition>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import AppNavigation from "@/components/AppNavigation.vue";
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld,
+    AppNavigation,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h2>Home Page</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ut, adipisci
+      molestias nisi iure porro ab nesciunt, quo ex id voluptates quas
+      repudiandae quis consequuntur consectetur laborum corrupti, eaque odit?
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+/* import HelloWorld from "@/components/HelloWorld.vue";
+import AppNavigation from "@/components/AppNavigation.vue"; */
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
